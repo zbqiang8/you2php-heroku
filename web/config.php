d0841d688f3891624575af49c7e37ce9d09a6127
zbqiang8 <398490841@qq.com>
2018-03-09 14:52:57 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', getenv("AIzaSyCSE5BNDfx-hIsy78KXcWH6_kmrrYDK4Kw"));
+define('GJ_CODE', getenv("HK"));
+define('SITE_NAME',getenv("不要说话"));
+define('TITLENAME', getenv("不要说话"));
+define('EN2DEKEY',getenv("iloveyou"));
+define('EMAIL', getenv("398490841@.com"));
 ?>
